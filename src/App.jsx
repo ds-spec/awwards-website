8461cad495763dced85d934f2f0cf9c494891587
Deepak <ds262969@gmail.com>
2025-02-02 21:36:37 +0530

    Hero Section Completed With GSAP Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-blue-50 font-bold font-circular-web">
-        Welcome to awwwards
-      </h1>
+    <main className="relative w-screen min-h-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-300" />
     </main>
   );
 };
